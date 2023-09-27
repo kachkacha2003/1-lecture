7ad2ed63102fed3a3c61ecd321b957f5451fb4e4
kachkacha2003 <lasha.katchkatchishvili.1@btu.edu.ge>
2023-09-27 21:33:34 +0400

    commit

---
+++ app.js
@@ -0,0 +1,49 @@
+// 1)დაწერე პროგრამა რომელიც კონსოლში გამოიტანს "Hello World" -ს.
+
+//Write a program to print "Hello, World!" to the console. 
+
+//console.log(7%5);
+
+// 2)შექმენით ცვლადი და მიანიჭეთ მას რიცხვი. დაბეჭდეთ ცვლადის მნიშვნელობა.
+
+//Create a variable and assign a number to it. Print the value of the variable.
+// var,let ,const
+// let x="hello world";
+// let y=5;
+// var word="ahaaha"
+// const number=10;
+// console.log(x);
+// console.log("x")
+// console.log(word);
+
+//3)დაწერეთ პროგრამა ორი რიცხვის შესაკრებად და შედეგის გამოსატანად.
+
+// Write a program to add two numbers and display the result. 
+
+// console.log(5+8);
+// let firstNumber=10;
+// let secondNumber=20;
+
+// console.log(firstNumber+secondNumber);
+
+// )დაწერეთ პროგრამა, რომელიც გამოაკლებს ორ რიცხვს ერთმანეთს და აჩვენებს შედეგს.
+
+//Write a program to subtract, two numbers and display the result. 
+
+// let num1=6;
+// let num2=7;
+// console.log(num2%num1);  -,*,/,
+
+// შექმენით ცვლადი და მიანიჭეთ მას სტრიქონი. ამობეჭდეთ სტრიქონის სიგრძე.
+// console.log("rjrorhhrhhr ".length)
+// let word="bitcamp";
+// console.log("bit ca m p".length);
+
+// console.log("hello" + " "  + "world");
+
+//  10)დაწერეთ პროგრამა სტრიქონის დიდ ასოებზე გადასაყვანად.
+
+//Write a program to convert a string to uppercase. 
+
+// let X="Bitcamp"
+// console.log(X.toUpperCase());
